3e5ecbed8a0fdb19bc2170ec9fc75894baa0c3cc
shinglokman <shinglokman@gmail.com>
2025-04-15 01:41:33 +0800

    speakers pic

--- app/speakers/page.js
+++ app/speakers/page.js
@@ -17,16 +17,60 @@ const page = () => {
               <div className="speaker-page__single">
                 <div className="speaker-page__img-box">
                   <div className="speaker-page__img">
-                    <a href=""><img src="assets/images/speakers/william_yu.jpg" style={{height: '418px'}} alt /></a>
+                    <a href=""><img src="assets/images/speakers/allinnettes_go_adigue.jpg" style={{height: '418px'}} alt /></a>
                   </div>
                   <div className="speaker-page__img-shape" />
                 </div>
                 <div className="speaker-page__content">
                   <h3>
-                    <Link href="">Dr. William Yu</Link>
+                    <Link href="">Dr. Allinnettes Go Adigue</Link>
                   </h3>
-                  <p>Founder and Chief Executive Officer,</p>
-                  <p>World Green Organization</p>
+                  <p>Head of ASEAN Regional Hub,</p>
+                  <p>Global Reporting Initiative</p>
+                </div>
+              </div>
+            </div>
+            {/*Speaker Page Single End*/}
+            {/*Speaker Page Single Start*/}
+            <div
+              className="col-xl-4 col-lg-4 wow fadeInLeft"
+              data-wow-delay="100ms"
+            >
+              <div className="speaker-page__single">
+                <div className="speaker-page__img-box">
+                  <div className="speaker-page__img">
+                    <a href=""><img src="assets/images/speakers/candice_dott.jpg" style={{height: '418px'}} alt /></a>
+                  </div>
+                  <div className="speaker-page__img-shape" />
+                </div>
+                <div className="speaker-page__content">
+                  <h3>
+                    <Link href="">Ms. Candice Dott</Link>
+                  </h3>
+                  <p>Director of Market Engagement,</p>
+                  <p>TNFD</p>
+                </div>
+              </div>
+            </div>
+            {/*Speaker Page Single End*/}
+            {/*Speaker Page Single Start*/}
+            <div
+              className="col-xl-4 col-lg-4 wow fadeInLeft"
+              data-wow-delay="100ms"
+            >
+              <div className="speaker-page__single">
+                <div className="speaker-page__img-box">
+                  <div className="speaker-page__img">
+                    <a href=""><img src="assets/images/speakers/cloris_cai.jpg" style={{height: '418px'}} alt /></a>
+                  </div>
+                  <div className="speaker-page__img-shape" />
+                </div>
+                <div className="speaker-page__content">
+                  <h3>
+                    <Link href="">Ms. Cloris Cai</Link>
+                  </h3>
+                  <p>Sustainability Solutions Manager,</p>
+                  <p>Green Initiatives, Intertek China</p>
                 </div>
               </div>
             </div>
@@ -83,16 +127,82 @@ const page = () => {
               <div className="speaker-page__single">
                 <div className="speaker-page__img-box">
                   <div className="speaker-page__img">
+                    <a href=""><img src="assets/images/speakers/jean_sebastien_jacquetin.jpg" style={{height: '418px'}} alt /></a>
+                  </div>
+                  <div className="speaker-page__img-shape" />
+                </div>
+                <div className="speaker-page__content">
+                  <h3>
+                    <Link href="">Mr. Jean-Sébastien Jacquetin</Link>
+                  </h3>
+                  <p>Managing Partner,</p>
+                  <p>Cavendish Investment Corporation</p>
+                </div>
+              </div>
+            </div>
+            {/*Speaker Page Single End*/}
+            {/*Speaker Page Single Start*/}
+            <div
+              className="col-xl-4 col-lg-4 wow fadeInLeft"
+              data-wow-delay="100ms"
+            >
+              <div className="speaker-page__single">
+                <div className="speaker-page__img-box">
+                  <div className="speaker-page__img">
                     <a href=""><img src="assets/images/speakers/kin_sang_cheung.jpg" style={{height: '418px'}} alt /></a>
                   </div>
                   <div className="speaker-page__img-shape" />
                 </div>
                 <div className="speaker-page__content">
                   <h3>
-                    <Link href="">Mr. Cheung Kin Sang</Link>
+                    <Link href="">Mr. Kin Sang Cheung</Link>
                   </h3>
                   <p>Chairman, Board of Governors,</p>
-                  <p>World Green Organization</p>
+                  <p>World Green Organisation</p>
+                </div>
+              </div>
+            </div>
+            {/*Speaker Page Single End*/}
+            {/*Speaker Page Single Start*/}
+            <div
+              className="col-xl-4 col-lg-4 wow fadeInLeft"
+              data-wow-delay="100ms"
+            >
+              <div className="speaker-page__single">
+                <div className="speaker-page__img-box">
+                  <div className="speaker-page__img">
+                    <a href=""><img src="assets/images/speakers/mei_zi_tan.jpg" style={{height: '418px'}} alt /></a>
+                  </div>
+                  <div className="speaker-page__img-shape" />
+                </div>
+                <div className="speaker-page__content">
+                  <h3>
+                    <Link href="">Ms. Mei Zi Tan</Link>
+                  </h3>
+                  <p>Manager, International Research & Projects,</p>
+                  <p>Carbon Market Institute (CMI)</p>
+                </div>
+              </div>
+            </div>
+            {/*Speaker Page Single End*/}
+            {/*Speaker Page Single Start*/}
+            <div
+              className="col-xl-4 col-lg-4 wow fadeInLeft"
+              data-wow-delay="100ms"
+            >
+              <div className="speaker-page__single">
+                <div className="speaker-page__img-box">
+                  <div className="speaker-page__img">
+                    <a href=""><img src="assets/images/speakers/ryan_cook.jpg" style={{height: '418px'}} alt /></a>
+                  </div>
+                  <div className="speaker-page__img-shape" />
+                </div>
+                <div className="speaker-page__content">
+                  <h3>
+                    <Link href="">Mr. Ryan Cook</Link>
+                  </h3>
+                  <p></p>
+                  <p></p>
                 </div>
               </div>
             </div>
@@ -119,6 +229,73 @@ const page = () => {
               </div>
             </div>
             {/*Speaker Page Single End*/}
+            {/*Speaker Page Single Start*/}
+            <div
+              className="col-xl-4 col-lg-4 wow fadeInLeft"
+              data-wow-delay="100ms"
+            >
+              <div className="speaker-page__single">
+                <div className="speaker-page__img-box">
+                  <div className="speaker-page__img">
+                    <a href=""><img src="assets/images/speakers/sun_ta_chien.jpg" style={{height: '418px'}} alt /></a>
+                  </div>
+                  <div className="speaker-page__img-shape" />
+                </div>
+                <div className="speaker-page__content">
+                  <h3>
+                    <Link href="">Dr. Sun Ta Chien</Link>
+                  </h3>
+                  <p>Executive Director,</p>
+                  <p>Digital Domain</p>
+                </div>
+              </div>
+            </div>
+            {/*Speaker Page Single End*/}
+            {/*Speaker Page Single Start*/}
+            <div
+              className="col-xl-4 col-lg-4 wow fadeInLeft"
+              data-wow-delay="100ms"
+            >
+              <div className="speaker-page__single">
+                <div className="speaker-page__img-box">
+                  <div className="speaker-page__img">
+                    <a href=""><img src="assets/images/speakers/toa_charm.jpg" style={{height: '418px'}} alt /></a>
+                  </div>
+                  <div className="speaker-page__img-shape" />
+                </div>
+                <div className="speaker-page__content">
+                  <h3>
+                    <Link href="">Dr.Toa Charm</Link>
+                  </h3>
+                  <p>Associate Professor, Practice in Innovation and Technology,</p>
+                  <p>The Chinese University of Hong Kong</p>
+                </div>
+              </div>
+            </div>
+            {/*Speaker Page Single End*/}
+            {/*Speaker Page Single Start*/}
+            <div
+              className="col-xl-4 col-lg-4 wow fadeInLeft"
+              data-wow-delay="100ms"
+            >
+              <div className="speaker-page__single">
+                <div className="speaker-page__img-box">
+                  <div className="speaker-page__img">
+                    <a href=""><img src="assets/images/speakers/william_yu.jpg" style={{height: '418px'}} alt /></a>
+                  </div>
+                  <div className="speaker-page__img-shape" />
+                </div>
+                <div className="speaker-page__content">
+                  <h3>
+                    <Link href="">Dr. William Yu</Link>
+                  </h3>
+                  <p>Founder & Chief Executive Officer,</p>
+                  <p>World Green Organization</p>
+                </div>
+              </div>
+            </div>
+            {/*Speaker Page Single End*/}
+            
           </div>
         </div>
       </section>
